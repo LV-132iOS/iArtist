c9bd4ec9dc492c67b11b6af52618ac8fccb061a4
LV-132iOS <clark@mactr1.training.local>
2015-02-20 17:05:18 +0200

    Commit

--- iArtist/PreviewOnWallViewController.m
+++ iArtist/PreviewOnWallViewController.m
@@ -70,7 +70,7 @@ typedef NS_ENUM(NSInteger, AVTypeOfPictureChange){
     
     Wall *wall1 = [Wall new];
     wall1.wallPicture = [UIImage imageNamed:@"room1.jpg"];
-    wall1.distanceToWall = @1.5;
+    wall1.distanceToWall = @1.0;
     self.currentWall = wall1;
 }
 
